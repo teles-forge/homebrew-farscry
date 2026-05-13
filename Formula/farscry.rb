3fb7d04a07e01c8e3a076a9a529b9abb746e7afa
Teles Nascimento <123102867+TelesNascimento25@users.noreply.github.com>
2026-05-13 17:50:23 -0300

    feat: add farscry formula v0.1.0

---
+++ Formula/farscry.rb
@@ -0,0 +1,21 @@
+class Farscry < Formula
+  desc "Vision APIs describe. farscry gives coordinates. Agents that act, not guess."
+  homepage "https://farscry.dev"
+  version "0.1.0"
+  license "Apache-2.0"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/teles-forge/farscry/releases/download/v#{version}/farscry-aarch64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER_ARM64"
+    end
+  end
+
+  def install
+    bin.install "farscry-aarch64-apple-darwin/farscry"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/farscry --version 2>&1")
+  end
+end
